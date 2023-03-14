5308575256864c05776291e6175572e0e16c8e12
user2 <user2@gmail.com>
2023-03-14 09:56:46 +0100

    tp2 etape 3.1

---
+++ SitePro/v2/index.php
@@ -0,0 +1,20 @@
+<html>
+
+    <head>
+        <title> Exemple Dynamique </title>
+    </head>
+    
+    <body>
+        La date d'aujourd'hui est le :
+        <?php
+            // Affichage de la date
+            echo date("d/m/Y");
+        ?>
+        <?php
+            $user = "IMT";
+            echo "<br>";
+            echo "Hello, " . $user;
+        ?>
+    </body>
+
+</html>
