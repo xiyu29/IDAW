c6740dccc2f41e96e3e379554382817c8ed5f307
user2 <user2@gmail.com>
2023-03-14 16:20:36 +0100

    TP2 Q6 debut

---
+++ SitePro/v3/post.php
@@ -0,0 +1,10 @@
+<?php
+require_once('template_header.php');
+?>
+
+        <h1 align="center">Je ne sais pas encore ce que à mettre dans cette page</h1>
+        <h1 align="center"><a href="index.php">Return to home page</a></h1>
+
+<?php
+require_once('template_foot.php');
+?>
---
+++ SitePro/v3/twitter.php
@@ -0,0 +1,10 @@
+<?php
+require_once('template_header.php');
+?>
+
+        <h1 class="center">Je n'ai pas de compte Twitter</h1>
+        <h1 class="center"><a href="index.php">Return to home page</a></h1>
+
+<?php
+require_once('template_foot.php');
+?>
