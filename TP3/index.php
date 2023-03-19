214c4d207863ba809cb5fff273ccc28e4f6ff0c9
user2 <user2@gmail.com>
2023-03-19 16:35:20 +0100

    cookie style_choisi ameliore

--- TP3/index.php
+++ TP3/index.php
@@ -2,9 +2,19 @@
     if(isset($_GET['css'])){
         setcookie('style_choisi', $_GET['css']);
         $_COOKIE['style_choisi'] = $_GET['css'];
-        echo '<link rel="stylesheet" href="' . $_COOKIE['style_choisi'] . '.css"/>';   
+        echo '<link rel="stylesheet" href="' . $_COOKIE['style_choisi'] . '.css"/>'; 
+        echo 'Cookie : ' . $_COOKIE['style_choisi']; 
     }
-    echo 'Cookie : ' . $_COOKIE['style_choisi'];
+
+    if(!isset($_COOKIE['style_choisi'])){
+        $_COOKIE['style_choisi'] = 'style1';
+        echo '<link rel="stylesheet" href="' . $_COOKIE['style_choisi'] . '.css"/>';
+        echo 'cookie auto generated';
+    }
+
+    if(isset($_POST['clearCookie'])){
+        setcookie('style_choisi', '', time() - 3600);
+    }  
 ?>
 
 <form id="style_form" action="index.php" method="GET">
@@ -17,7 +27,17 @@
 
 <form method="post" action="login.php">
     <input type="submit" name="submit" value="Login">
-</form> 
+</form>
+
+<form method="post" action="index.php">
+    <input type="submit" name="clearCookie" value="Clear cookie">
+</form>
+
+<?php
+
+    
+
+?>
 
 
 
