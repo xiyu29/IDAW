b46833b359afb5968af2947c7d2895453736f13b
user2 <user2@gmail.com>
2023-03-20 10:51:37 +0100

    TP4 Q2 ok

--- TP4/users.php
+++ TP4/users.php
@@ -20,43 +20,41 @@
 
     $request = $pdo->prepare("select * from users");
     $request->execute();
-    $result = $request->fetchAll();
-    $length = count($result);
-    $sousLength = count($result[0]) / 2;
+    $result = $request->fetchAll(PDO::FETCH_OBJ);
+    // $length = count($result);
+    // $sousLength = count($result[0]) / 2;
     // print_r($length);
     // print_r($result[0]);
     // print_r($sousLength);
     // print_r($result);
 
     echo '<h1 align="center">Users</h1>';
-    echo '<table align="center">
-            <thead>
-                <tr>
-                    <th width="100px">id</th>
-                    <th width="100px">name</th>
-                    <th width="300px">email</th>
-                </tr>
-            </thrad>
-            <tbody>';
-    for($i = 0; $i < $length; $i ++){
-        $sousLigne = $result[$i];
-        echo '<tr align="center">';
-        for($j = 0; $j < $sousLength; $j ++){
-            echo "<td>$sousLigne[$j]</td>";
-        }
-        echo '</tr>';  
+    echo 
+        '
+            <table align="center">
+                <thead>
+                    <tr>
+                        <th width="100px">id</th>
+                        <th width="100px">name</th>
+                        <th width="300px">email</th>
+                    </tr>
+                </thrad>
+                <tbody>
+        ';
+    foreach($result as $value){
+        echo
+            '<tr align="center">'.
+                '<td>' . $value->id . '</td>'.
+                '<td>' . $value->name . '</td>'.
+                '<td>' . $value->email . '</td>'.
+            '</tr>'
+        ;
     }
-    // while($row = mysqli_fetch_array($request, MYSQLI_ASSOC))
-    // {
-    //     echo 
-    //         "<tr align='center'><td> {$row['id']}</td> ".
-    //         "<td>{$row['name']} </td> ".
-    //         "<td>{$row['email']} </td> ".
-    //         "</tr>";
-    // }
-    echo '
-            </tboday>
-        </table>';
+    echo 
+        '
+                </tboday>
+            </table>
+        ';
     
     $pdo = null;
 ?>
